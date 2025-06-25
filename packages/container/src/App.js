d108d66deace3dccacf0473aa3b3bad1383de581
Jaime Cruz <jaime.cruz@globant.com>
2025-06-25 12:18:59 -0600

    marketing deployment 5 css 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,13 +3,23 @@ import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
   return (
+    
     <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header />
         <MarketingApp />
       </div>
+      </StylesProvider>
     </BrowserRouter>
+    
   );
 };
